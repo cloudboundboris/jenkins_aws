cb517cc9208fad443fa17ef21fda1a291211a235
Boris Levenzon <blevenzon@gmail.com>
2023-06-10 21:05:03 -0400

    Configured DNS Records and Certs with TF

--- output.tf
+++ output.tf
@@ -7,6 +7,12 @@ output "ec2_public_ip" {
 output "aws_efs_id" {
   description = "EFS ID"
   value       = aws_efs_file_system.efs1.id
+
+}
+
+output "elb_dns_name" {
+  description = "ELB DNS Name"
+  value       = module.elb.elb_dns_name
 }
 
 
--- security.tf
+++ security.tf
@@ -52,3 +52,30 @@ resource "aws_security_group" "efs-sg" {
     Environment = "dev"
   }
 }
+
+resource "aws_security_group" "elb" {
+  vpc_id = aws_vpc.vpc1.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  dynamic "ingress" {
+    for_each = var.PORTS_ELB
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  tags = {
+    Name        = "${var.PROJECT_NAME}_sg_elb"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+
+}
